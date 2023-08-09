0762c8b69a519a4c63dcc3120cfd58049d0adc32
Chris Kim <hjchristopher.dev@gmail.com>
2023-08-09 14:38:12 -0700

    Implement navigationRef and extract form logic into a separate component

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://69ff-2604-3d08-9582-a100-3899-fb3d-bfa6-2a72.ngrok-free.app'
+    baseURL: 'https://f104-2604-3d08-9582-a100-d02b-bc04-fc0e-8672.ngrok-free.app'
 });
