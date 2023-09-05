c4522d48a788f89cf2acca8d35fd3918dce029fa
Chris Kim <hjchristopher.dev@gmail.com>
2023-09-05 12:44:12 -0700

    Implement saving tracks feature

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
-    baseURL: 'https://6ad7-2604-3d08-9582-a100-cc18-f158-eb26-29f5.ngrok-free.app'
+const instance = axios.create({
+    baseURL: 'https://f930-2604-3d08-9582-a100-595e-8261-981c-6b12.ngrok-free.app'
 });
+
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (err) => {
+        return Promise.reject(err);
+    }
+);
+
+export default instance;
